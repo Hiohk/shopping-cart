0fd691984ea6b01eb9ca8349af4cc52ac0a6c6d2
lovepei <hk_snnu2020@163.com>
2022-07-09 10:44:03 +0800

    完成购物车

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,98 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app-container">
+    <MyHeader title="购物车"></MyHeader>
+    <Goods
+      v-for="item in list"
+      :key="item.id"
+      :id="item.id"
+      :title="item.goods_name"
+      :pic="item.goods_img"
+      :price="item.goods_price"
+      :state="item.goods_state"
+      :count="item.goods_count"
+      @state-change="getNewState"
+    ></Goods>
+    <Footer
+      :isfull="fullState"
+      :amount="amount"
+      :all="total"
+      @full-change="getFullState"
+    ></Footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
+import bus from "@/components/eventBus.js";
+import MyHeader from "@/components/Header/Header.vue";
+import Goods from "@/components/Goods/Goods.vue";
+import Footer from "@/components/Footer/Footer.vue";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    MyHeader,
+    Goods,
+    Footer,
+  },
+  data() {
+    return {
+      list: [],
+    };
+  },
+  created() {
+    this.initCartList();
+    bus.$on("share", (val) => {
+      this.list.some((item) => {
+        if (item.id === val.id) {
+          item.goods_count = val.value;
+          return true;
+        }
+      });
+    });
+  },
+  computed: {
+    //动态计算出全选的状态
+    fullState() {
+      return this.list.every((item) => item.goods_state);
+    },
+    //选中的商品总价格
+    amount() {
+      return this.list
+        .filter((item) => item.goods_state)
+        .reduce((total, item) => (total += item.goods_price * item.goods_count), 0);
+    },
+    //选中的商品总数量
+    total() {
+      return this.list
+        .filter((item) => item.goods_state)
+        .reduce((total, item) => (total += item.goods_count), 0);
+    },
+  },
+  methods: {
+    async initCartList() {
+      const { data: res } = await axios.get("https://www.escook.cn/api/cart");
+      if (res.status === 200) {
+        this.list = res.list;
+      }
+    },
+    getNewState(val) {
+      this.list.some((item) => {
+        if (item.id === val.id) {
+          item.goods_state = val.value;
+        }
+      });
+    },
+    getFullState(val) {
+      this.list.forEach((item) => (item.goods_state = val));
+    },
+  },
+};
 </script>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="less" scoped>
+.app-container {
+  padding-top: 45px;
+  padding-bottom: 50px;
+  height: 100%;
 }
 </style>
